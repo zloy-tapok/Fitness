e49ac883b30199c8421e5f76e77fd2a001180ea7
zloy-tapok <odisseik@gmail.com>
2020-04-23 10:59:43 +0900

    23.04.1100

---
+++ Fitness.BL/Control/EatingController.cs
@@ -0,0 +1,38 @@
+﻿using Fitness.BL.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Control
+{
+     public class EatingController
+    {
+        private readonly User user;
+        public List<Food> Foods
+
+        public EatingController(User user)
+        {
+            this.user = user ?? throw new ArgumentNullException("Пользователь не может быть пустым", nameof(user));
+            Foods = GetAllFoods();
+        }
+        private List<Food> GetAllFoods()
+        {
+            var formatter = new BinaryFormatter();
+            using (var fs = new FileStream("foods.dat", FileMode.OpenOrCreate))
+            {
+                if (fs.Length > 0 && formatter.Deserialize(fs) is List<Food> foods)
+                {
+                    return foods;
+                }
+                else
+                {
+                    return new List<Food>();
+                }
+            }
+        }
+    }
+}

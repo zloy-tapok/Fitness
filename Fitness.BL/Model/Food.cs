aafb36b8899f1e7c05fa105f534478c25c43ea12
zloy-tapok <odisseik@gmail.com>
2020-04-23 10:29:47 +0900

    1

---
+++ Fitness.BL/Model/Food.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Fitness.BL.Model
+{
+	public class Food
+	{
+		public string Name { get; }
+		/// <summary>
+        /// Белки.
+        /// </summary>
+	    public double Proteins { get; }
+
+		/// <summary>
+		/// Жиры.
+		/// </summary>
+		public double Fats { get; }
+
+		/// <summary>
+		/// Углеводы.
+		/// </summary>
+		public double Carbs { get; }
+
+		/// <summary>
+		/// Калории за 100 грамм продукта.
+		/// </summary>
+		public double Calories { get; }
+
+		public Food(string name) : this (name, 0, 0, 0, 0) { }
+  
+		public Food(string name, double calories, double proteins, double fats, double carbs)
+		{
+            Name = name;
+            Calories = calories / 100.0;
+            Proteins = proteins / 100.0;
+            Fats = fats / 100.0;
+            Carbs = carbs / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
